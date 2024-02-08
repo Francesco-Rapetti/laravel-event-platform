39a6e4ea6f488eeed33758eb5e90bde6d05a72a8
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-08 15:08:50 +0100

    Import events data

---
+++ config/events.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    [
+        'name' => 'Event 1',
+        'description' => 'Event 1 description',
+        'date' => '2022-01-01',
+        'available_tickets' => 10
+    ],
+    [
+        'name' => 'Event 2',
+        'description' => 'Event 2 description',
+        'date' => '2022-01-02',
+        'available_tickets' => 20
+    ],
+    [
+        'name' => 'Event 3',
+        'description' => 'Event 3 description',
+        'date' => '2022-01-03',
+        'available_tickets' => 30
+    ],
+    [
+        'name' => 'Event 4',
+        'description' => 'Event 4 description',
+        'date' => '2022-01-04',
+        'available_tickets' => 40
+    ]
+];
