42f0874e8e2a1e00979176664e2c07fbc857e6c0
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-08 15:08:03 +0100

    Refactor dashboard controller template

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Project;
-use App\Models\Type;
+use App\Models\Event;
+use App\Models\Tag;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
-        $types = Type::all();
-        return view('admin.dashboard', compact('projects', 'types'));
+        $events = Event::all();
+        $tags = Tag::all();
+        return view('admin.dashboard', compact('events', 'tags'));
     }
 }
