b7bdc411a66b296c2a4ad4d2a04e38ea9207dbcd
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-09 12:06:37 +0100

    Feat: implement tag edit view

---
+++ resources/views/admin/Tags/edit.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid mt-4">
+        <div class="row justify-content-center">
+            <h2>Edit {{ $tag->name }} tag</h2>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+        <div class="row">
+            <form action="{{ route('admin.tags.update', $tag) }}" method="POST">
+                @csrf
+                @method('PUT')
+                {{-- name description --}}
+                <div class="mb-3">
+                    <label for="name" class="form-label">name</label>
+                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                        name="name" value="{{ old('name') ?? $tag->name }}">
+                    @error('name')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="description" class="form-label">description</label>
+                    <input type="text" class="form-control @error('description') is-invalid @enderror" id="description"
+                        name="description" value="{{ old('description') ?? $tag->description }}">
+                    @error('description')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="color" class="form-label">Seleziona il colore</label>
+                    <select name="color" id="color" class="form-select" multiple>
+                        @foreach ($colors as $color)
+                            <option {{ $tag->color == $color ? 'selected' : '' }} value="{{ $color }}">
+                                {{ $color }}</option>
+                        @endforeach
+                    </select>
+                    @error('color')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <button type="submit" class="btn btn-primary">Insert</button>
+            </form>
+        </div>
+    </div>
+@endsection
