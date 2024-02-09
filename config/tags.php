24f15f5e1a638ac90f0bd14f63ec999de7ad313a
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-09 12:04:37 +0100

    Import tag data

---
+++ config/tags.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    [
+        'name' => 'Concert',
+        'description' => 'Live concert',
+        'color' => 'bg-secondary'
+    ],
+    [
+        'name' => 'Exhibition',
+        'description' => 'Exhibition event in the city',
+        'color' => 'bg-danger'
+    ],
+    [
+        'name' => 'Expo',
+        'description' => 'Exhibition event outside the city',
+        'color' => 'bg-success'
+    ],
+    [
+        'name' => 'Food',
+        'description' => 'Food event',
+        'color' => 'bg-primary-subtle text-dark'
+    ],
+    [
+        'name' => 'Conference',
+        'description' => 'Conference event',
+        'color' => 'bg-warning text-dark'
+    ],
+    [
+        'name' => 'Computer Science',
+        'description' => 'Computer Science masterclass',
+        'color' => 'bg-danger-subtle text-dark'
+    ],
+    [
+        'name' => 'Live',
+        'description' => 'Live event',
+        'color' => 'bg-info-subtle text-dark'
+    ],
+    [
+        'name' => 'Streaming',
+        'description' => 'Streaming event',
+        'color' => 'bg-info'
+    ]
+];
