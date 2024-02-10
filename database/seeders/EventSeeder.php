26866f5fb912dacf15df21c4fc57aa8af01658a2
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-10 14:41:42 +0100

    Feat: generate fake events

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -5,18 +5,30 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Event;
+use Faker\Generator as Faker;
 
 class EventSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        $events = config("events");
-        foreach ($events as $event) {
+        // $events = config("events");
+        // foreach ($events as $event) {
+        //     $newEvent = new Event();
+        //     $newEvent->fill($event);
+        //     $newEvent->save();
+        // }
+
+        for ($i = 0; $i < 6; $i++) {
             $newEvent = new Event();
-            $newEvent->fill($event);
+            $newEvent->fill([
+                "name" => $faker->sentence(3),
+                "description" => $faker->sentence(10),
+                "date" => $faker->dateTime(),
+                "available_tickets" => $faker->numberBetween(1, 100),
+            ]);
             $newEvent->save();
         }
     }
